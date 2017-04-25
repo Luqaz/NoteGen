a99303352a980263f82773f71df6da120334065c
Luqaz <igor.shakun.dev@gmail.com>
2017-04-26 01:01:28 +0300

    Initial commit

--- NoteGen/MainWindow.xaml.cs
+++ NoteGen/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CSCore.Codecs;
+using Microsoft.Win32;
+using SoundProcessor;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
 
 namespace NoteGen
 {
@@ -20,9 +25,72 @@ namespace NoteGen
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<byte> fileBytes = new List<byte>();
+        private SoundPlayer player;
+        private string fileName { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void FileOpenHandler(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "MP3 File (*.mp3)|*.mp3|FLAC file (*.flac)|*.flac|ALAC file (*.m4a)|*.m4a";
+            if(fileDialog.ShowDialog().Value)
+            {
+                fileName = fileDialog.FileName;
+                player = SoundPlayer.getInstance(fileName);
+                try
+                {
+                    using (var fileStream = fileDialog.OpenFile())
+                    {
+                        if(String.IsNullOrWhiteSpace(player.Info))
+                        {
+                            fileInfo.Content = fileDialog.SafeFileName;
+                        }
+                        else
+                        {
+                            fileInfo.Content = player.Info;
+                        }
+                        int fileByte = fileStream.ReadByte();
+                        while (fileByte != -1)
+                        {
+                            fileBytes.Add((byte)fileByte);
+                            fileByte = fileStream.ReadByte();
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message + "\n\n\n" + ex.StackTrace);
+                }
+                
+            }
+        }
+
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(player != null)
+            {
+                if(player.Status == "Playing")
+                {
+                    playButton.Content = "Play";
+                    player.Stop();
+                }
+                else
+                {
+                    playButton.Content = "Pause";
+                    player.Play();
+                }
+            }
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            player.Stop();
         }
     }
 }
---
+++ SoundProcessor/SoundPlayer.cs
@@ -0,0 +1,113 @@
+﻿//using CSCore;
+//using CSCore.Codecs;
+//using CSCore.SoundOut;
+using NAudio;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SoundProcessor
+{
+    public class SoundPlayer
+    {
+        private AudioFileReader audioFileReader;
+        private IWavePlayer waveOutDevice;
+        private static SoundPlayer player;
+        public string Status
+        {
+            get
+            {
+                return waveOutDevice.PlaybackState.ToString();
+            }
+        }
+
+        public string Info;
+
+
+        private SoundPlayer(string fileName)
+        {
+            waveOutDevice = new WaveOut();
+            getTag(fileName);
+            audioFileReader = new AudioFileReader(fileName);
+            var Tag = Id3v2Tag.ReadTag(audioFileReader);
+            //Info = GetInfo(Tag.RawData);
+        }
+
+        private void getTag(string fileName)
+        {
+            Info = "";
+            if (fileName.EndsWith(".mp3"))
+            {
+                var reader = new Mp3FileReader(fileName);
+                Info = GetInfo(reader.Id3v1Tag);
+            }
+            else
+            {
+                var reader = new MediaFoundationReader(fileName);
+                var tag = Id3v2Tag.ReadTag(reader);
+            }
+        }
+
+        public static SoundPlayer getInstance(string fileName)
+        {
+            if (player == null)
+            {
+                player = new SoundPlayer(fileName);
+            }
+            else
+            {
+                player.audioFileReader = new AudioFileReader(fileName);
+                player.getTag(fileName);
+                //player.Info = player.GetInfo(Tag.RawData);
+            }
+            return player;
+        }
+
+        public void Play()
+        {
+            waveOutDevice.Init(audioFileReader);
+            waveOutDevice.Play();
+        }
+
+        public void Stop()
+        {
+            waveOutDevice.Stop();
+        }
+
+        public Stream getRawAudioDataStream()
+        {
+            Stream rawAudio = new MemoryStream();
+            audioFileReader.CopyTo(rawAudio);
+            return rawAudio;
+        }
+
+        public string GetInfo(byte[] b)
+        {
+            string sTitle = "unknown";
+            string sSinger = "unknown";
+            string sAlbum = "unknown";
+            string sYear = "unknown";
+            string sComm = "unknown";
+
+            var isSet = false;
+            var sFlag = Encoding.Default.GetString(b, 0, 3);
+            if (sFlag.CompareTo("TAG") == 0)
+            {
+                isSet = true;
+            }
+            if (isSet)
+            {
+                sTitle = Encoding.Default.GetString(b, 3, 30);
+                sSinger = Encoding.Default.GetString(b, 33, 30);
+                sAlbum = Encoding.Default.GetString(b, 63, 30);
+                sYear = Encoding.Default.GetString(b, 93, 4);
+                sComm = Encoding.Default.GetString(b, 97, 30);
+            }
+
+            return String.Format("{0} - {1}", sSinger.Trim(), sTitle.Trim());
+        }
+    }
+}
